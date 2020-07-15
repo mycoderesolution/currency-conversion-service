a3017428d01b17c0b6ca606db6a84df091bdc394
praveshasiwal <impraveshasiwal@gmail.com>
2020-07-15 22:19:33 +0530

    first commit

---
+++ src/main/java/com/javatpoint/microservices/CurrencyConversionServiceApplication.java
@@ -0,0 +1,20 @@
+package com.javatpoint.microservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate getRestTemplate() {
+		return new RestTemplate();
+	}
+
+}
---
+++ src/main/java/com/javatpoint/microservices/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,40 @@
+package com.javatpoint.microservices.currencyconversionservice;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.support.BasicAuthorizationInterceptor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SuppressWarnings("deprecation")
+@RestController
+public class CurrencyConversionController {
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
+	public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to,
+			@PathVariable BigDecimal quantity) {
+
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("from", from);
+		uriVariables.put("to", to);
+
+		restTemplate.getInterceptors().add(new BasicAuthorizationInterceptor("admin", "admin"));
+		ResponseEntity<CurrencyConversionBean> responseEntity = restTemplate.getForEntity(
+				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversionBean.class,
+				uriVariables);
+
+		CurrencyConversionBean response = responseEntity.getBody();
+
+		return response;
+	}
+
+}
