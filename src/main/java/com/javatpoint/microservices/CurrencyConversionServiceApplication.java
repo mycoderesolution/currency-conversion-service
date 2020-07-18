4d41e8a3f8f145953aa02fa09a5dcf2f89355c95
praveshasiwal <impraveshasiwal@gmail.com>
2020-07-18 12:08:07 +0530

    Disable Security and Configure with Eureka Server

--- src/main/java/com/javatpoint/microservices/CurrencyConversionServiceApplication.java
+++ src/main/java/com/javatpoint/microservices/CurrencyConversionServiceApplication.java
@@ -2,11 +2,13 @@ package com.javatpoint.microservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableFeignClients("com.javatpoint.microservices.currencyconversionservice")
 public class CurrencyConversionServiceApplication {
 
